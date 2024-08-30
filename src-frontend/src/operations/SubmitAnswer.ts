2dd1b9f698f6c17cfbb0d470ecdc8e0dec38dfc7
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2024-08-30 17:04:27 +0100

    lowercase

--- src-frontend/src/operations/SubmitAnswer.ts
+++ src-frontend/src/operations/SubmitAnswer.ts
@@ -27,7 +27,7 @@ export default async function SubmitAnswer(answer:string)
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                data: answer
+                data: answer.toLowerCase()
             })
         });
         
