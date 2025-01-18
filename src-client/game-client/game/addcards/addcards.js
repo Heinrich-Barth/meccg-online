a005226cce987d005558b9d6792207d7570a12b4
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2025-01-18 14:41:41 +0000

    add cards to sideboard by searching

--- src-client/game-client/game/addcards/addcards.js
+++ src-client/game-client/game/addcards/addcards.js
@@ -1,74 +1,17 @@
 
 const AddCardsInGame = {
 
-    _added : false,
-
-    removeQuotes : function(sCode) 
+    toDeck : function(code)
     {
-        if (sCode.indexOf('"') === -1)
-            return sCode;
-        else
-            return sCode.replace(/"/g, '');
-    },
-
-    getCount : function(line)
-    {
-        const res = {
-            count: 0,
-            code : ""
-        };
-
-        line = line.trim();
-        if (line.length < 3)
-            return res;
+        if (typeof code !== "string" || code === "")
+            return [];
 
         try
         {
-            const left = line.substring(0, 2);
-            const val = parseInt(left);
-            if (isNaN(val))
-            {
-                res.count = 1;
-                res.code = line;
-            }
-            else if (left > 0)
-            {
-                
-            }
-        }
-        catch(err)
-        {
-            console.warn(err);
-        }
-
-        return 1;
-    },
-
-    getCode : function(line)
-    {
-        return this.removeQuotes(line);
-    },
-
-    toDeck : function(sText)
-    {
-        try
-        {
-            const jDeck = [];
-
-            for (let _entry of sText.trim().split('\n'))
-            {
-                const code = this.removeQuotes(_entry.trim());
-                if (code === "")
-                    continue;
-
-                jDeck.push({
+            return [{
                     code : code,
                     count : 1
-                });
-            }
-
-            if (jDeck.length > 0)
-                return jDeck;
+                }];
         }
         catch (err)
         {
@@ -76,7 +19,7 @@ const AddCardsInGame = {
         }
 
         document.body.dispatchEvent(new CustomEvent("meccg-notify-error", { "detail": "Could not convert your list to a deck." }));
-        return null;
+        return [];
     },
 
     onEvent : function()
@@ -85,27 +28,7 @@ const AddCardsInGame = {
 
         const elem = document.getElementById("add-cards-wrapper");
         elem.classList.remove("hide");
-        elem.querySelector("textarea").focus();
-    },
-
-    onAdd : function()
-    {
-        let sText = document.getElementById("add-cards-wrapper").querySelector("textarea").value.toLowerCase();
-        if (sText === undefined || sText.trim() === "")
-        {
-            document.body.dispatchEvent(new CustomEvent("meccg-notify-info", { "detail": Dictionary.get("importtosb_message_add_list", "Please add cards to the list.") }));
-            return false;
-        }
-
-        const jDeck = AddCardsInGame.toDeck(sText);
-        if (jDeck !== null)
-        {
-            AddCardsInGame.onClose();
-            document.body.dispatchEvent(new CustomEvent("meccg-notify-success", { "detail": Dictionary.get("importtosb_message_add_list_ok", "Cards were added to your sideboard.") }));
-            MeccgApi.send("/game/card/add", { cards: jDeck });        
-        }
-
-        return false;
+        elem.querySelector("input").focus();
     },
 
     onClose : function()
@@ -124,9 +47,9 @@ const AddCardsInGame = {
         div.innerHTML = `<div id="add-cards-overlay" class="config-panel-overlay"></div>
             <div class="add-cards-box blue-box">
                 <h2 data-translate-inner="importtosb_title">${Dictionary.get("importtosb_title", "Add cards to your Sideboard")}</h2>
-                <p data-translate-inner="importtosb_p">${Dictionary.get("importtosb_p", "Please open the deckbuilder and copy the cards to add here.")}</p>
-                <textarea name="cards_to_add" data-translate-placeholder="importtosb_tip" placeholder="${Dictionary.get("importtosb_tip", "copy card codes here, e.g. Gandalf [H] (TW)")}"></textarea>
-                <p>&nbsp;</p>
+                <p>Search for cards and click them to add a copy to your sideboard.</p>
+                <input type="text" class="config-panel-text" name="cards_to_add" data-translate-placeholder="importtosb_tip" placeholder="Search for card name here">
+                <div class="result" id="add-card-result"></div>
             </div>`;
 
         const jTarget = div.querySelector(".add-cards-box");
@@ -138,14 +61,7 @@ const AddCardsInGame = {
         button.onclick = this.onClose;
         jTarget.appendChild(button);
 
-        button = document.createElement("input");
-        button.setAttribute("type", "button");
-        button.setAttribute("class", "button buttonUpdate");
-        button.setAttribute("value", Dictionary.get("importtosb_button_add", "Add to sideboard"));
-        button.onclick = this.onAdd.bind(this);
-        jTarget.appendChild(button);
-
-        jTarget.querySelector("textarea").onkeyup = this.onOnChange;
+        jTarget.querySelector("input").onkeyup = this.onOnChange;
         const _div = div.querySelector(".config-panel-overlay");
         _div.onclick = this.onClose;
         
@@ -159,6 +75,42 @@ const AddCardsInGame = {
 
         if (typeof e.cancelBubble !== "undefined")
             e.cancelBubble = true;
+
+        if (e.target.value.length < 3)
+            return;
+
+        const container = document.getElementById("add-card-result");
+        if (container === null)
+            return;
+
+        const res = g_Game.CardList.searchByName(e.target.value.toLowerCase());        
+        DomUtils.removeAllChildNodes(container);
+
+        for (let card of res)
+        {
+            const img = document.createElement("img");
+            img.setAttribute("class", "card-icon");
+            img.setAttribute("src", card.image);
+            img.setAttribute("title", "Click to add " + card.code);
+            img.setAttribute("data-code", card.code);
+            img.onmouseover = CardPreview._doHoverOnGuard;        
+            img.onmouseout = CardPreview.hideAll;
+            img.onclick = AddCardsInGame.onImageClick;
+            container.appendChild(img);
+        }
+    },
+
+    onImageClick(e)
+    {
+        const img = e.target;
+        const code = img ? img.getAttribute("data-code") : "";
+        const jDeck = AddCardsInGame.toDeck(code);
+        if (jDeck.length !== 0)
+        {
+            document.body.dispatchEvent(new CustomEvent("meccg-notify-success", { "detail": Dictionary.get("importtosb_message_add_list_ok", code + " added to your sideboard.") }));
+            MeccgApi.send("/game/card/add", { cards: jDeck });
+            e.target.classList.add("added");    
+        }
     }
 };
 
