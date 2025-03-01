06cae7491eeb3ff32e77367d7dae51f6277681e1
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2025-03-01 19:48:17 +0000

    caching via serviceworker
    external images can but should not be cached, because at some point. the storage is full...

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,66 +3,206 @@
  * https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
  */
 const CACHE_NAME = "v4";
+const CACHE_CARDS = "v4Images";
 
 /**
  * List of urls to cache
  */
 const assets = []
 
-/**
- * Check if given request path is to be cached at all
- * @param {String} url 
- * @returns success state
- */
-const cacheRequestPath = function (url) 
+
+const STRATEGY_CACHEFIRST_IMAGE = 1;
+const STRATEGY_CACHEFIRST_LOCAL = 2;
+const STRATEGY_NETWORKFIRST = 3;
+const STRATEGY_IGNORE = 4;
+
+const getUri = function(url)
 {
-    return url.startsWith("/data/images") 
-        || url.startsWith("/media/maps")
-        || url.startsWith("/media/assets");
+    let pos = url.indexOf("//");
+    if (pos !== -1)
+        pos = url.indexOf("/", pos + 3); // http://w
+
+    if (pos === -1)
+        return "";
+
+    return url.substring(pos);
 }
 
-/**
- * Check if given event is to be cached (and do so) or not
- * @param {Object} event 
- * @returns success state
- */
-const cacheRequest = function (event) 
+const uriStartsWith = function(uri, paths)
 {
-    if (!event.request.url.startsWith(self.location.origin))
-        return false;
-    else
-        return cacheRequestPath(event.request.url.substring(self.location.origin.length));
+    for (let path of paths)
+    {
+        if (uri.startsWith(path))
+            return true;
+    }
+
+    return false;
 }
 
-/**
- * Cache request
- * @param {Object} request 
- * @param {Object} response 
- */
-const putInCache = async (request, response) => 
+const URIS_IMAGE_LOCAL = [
+    "/media/maps", 
+    "/media/assets/js",
+    "/media/assets/fonts",
+    "/media/personalisation/dice",
+    "/media/personalisation/backgrounds",
+    "/media/personalisation/sounds",
+    "/data/samplerooms",
+    "/data/backside",
+    "/data/card-not-found-"
+];
+
+const URIS_NETWORK_FIRST = [
+    /*
+    "/data/decks",
+    "/data/list/map",
+    "/data/list/underdeeps",
+    "/data/list/cards",
+    "/data/list/stages",
+    "/data/list/filters",
+    "/data/list/avatars",
+    "/data/list/images",
+    "/static/frontend",
+    */
+]
+
+const identifyCacheStrategy = function(event)
+{
+    //if (event.request.url.startsWith("https://raw.githubusercontent.com") && event.request.url.endsWith(".jpg"))
+    //    return STRATEGY_CACHEFIRST_IMAGE;
+    
+    const uri = getUri(event.request.url);
+    if (uri.startsWith("/cards"))
+        return STRATEGY_CACHEFIRST_IMAGE;
+
+    if (uriStartsWith(uri, URIS_IMAGE_LOCAL))
+        return STRATEGY_CACHEFIRST_LOCAL;
+
+    if (uriStartsWith(uri, URIS_NETWORK_FIRST))
+        return STRATEGY_NETWORKFIRST;
+
+    return STRATEGY_IGNORE
+}
+
+const requestImage = async function(request)
+{
+    if (!request.url.startsWith("https://raw.git"))
+        return fetch(request.clone());
+    
+    const networkResponse = await fetch(request.clone(), { mode: "no-cors" });
+    if ( networkResponse.type !== "opaque" && networkResponse.ok === false ) 
+        throw new Error("Cannot fetch image");   
+
+    return networkResponse;
+}
+
+const networkFirst = async (request, preloadResponsePromise) => 
 {
     const cache = await caches.open(CACHE_NAME);
-    await cache.put(request, response);
-};
+
+    try
+    {
+        const responseFromNetwork = await fetch(request.clone(), { mode: 'cors' });
+        if (responseFromNetwork.ok) 
+        {
+            await updateCache(cache, request, responseFromNetwork);
+            return responseFromNetwork;
+        }
+    }
+    catch (err)
+    {
+        console.error(err.message);
+    }
+
+    if (preloadResponsePromise)
+    {
+        try
+        {
+            /* Try to use and cache the preloaded response, if it's there */
+            const preloadResponse = await preloadResponsePromise;
+            if (preloadResponse) 
+            {
+                await updateCache(cache, request, preloadResponse);
+                return preloadResponse;
+            }
+        }
+        catch (err)
+        {
+            console.error(err.message);
+        }    
+    }
+
+    return await cache.match(request);
+}
+
+const cacheCardImageFirst = async ({ request, preloadResponsePromise, _fallbackUrl }) => 
+{
+    const cache = await caches.open(CACHE_CARDS);
+    const responseFromCache = await cache.match(request);
+    if (responseFromCache)
+        return responseFromCache;
+
+    if (preloadResponsePromise)
+    {
+        try
+        {
+            /* Try to use and cache the preloaded response, if it's there */
+            const preloadResponse = await preloadResponsePromise;
+            if (preloadResponse) 
+            {
+                await updateCache(cache, request, preloadResponse);
+                return preloadResponse;
+            }
+        }
+        catch (err)
+        {
+            console.error(err.message);
+        }    
+    }
+
+    try
+    {
+        const networkResponse = await requestImage(request);
+        await updateCache(cache, request, networkResponse);
+        return networkResponse;    
+    }
+    catch(err)
+    {
+        console.warn(err.message);
+    }
+    
+    return fetch("/data/card-not-found-generic");
+}
+
+const updateCache = async function(cache, request, networkResponse)
+{
+    try
+    {
+        await cache.put(request, networkResponse.clone());
+    }
+    catch (err)
+    {
+        console.warn(err.message);
+    }
+}
 
 /**
  * Try to obtain object from cache first. If it does not exist, fetch and cache it
  * @param {Objects} param0 
  * @returns Response
  */
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => 
+const cacheFirst = async ({ request, preloadResponsePromise }) => 
 {
     /* Check cache first */
-    const responseFromCache = await caches.match(request);
+    const cache = await caches.open(CACHE_NAME);
+    const responseFromCache = await cache.match(request);
     if (responseFromCache)
         return responseFromCache;
-
 
     /* Try to use and cache the preloaded response, if it's there */
     const preloadResponse = await preloadResponsePromise;
     if (preloadResponse) 
     {
-        putInCache(request, preloadResponse.clone());
+        await updateCache(cache, request, preloadResponse);
         return preloadResponse;
     }
 
@@ -73,7 +213,7 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) =>
         if (responseFromNetwork.ok) 
         {
             /* response may be used only once we need to save clone to put one copy in cache and serve second one*/
-            putInCache(request, responseFromNetwork.clone());
+            await updateCache(cache, request, responseFromNetwork);
             return responseFromNetwork;
         }
     }
@@ -82,14 +222,9 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) =>
         console.warn(error.message);
     }
 
-    /* Fetch impossible. Use fallback  */
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse)
-        return fallbackResponse;
-
     /* generic error. we cannot do anything */
     return new Response("Network error happened", {
-        status: 408,
+        status: 404,
         headers: { "Content-Type": "text/plain" },
     });
 };
@@ -100,20 +235,84 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) =>
  */
 const fetchListener = function (event) 
 {
-    if (cacheRequest(event)) 
+    const strategy = identifyCacheStrategy(event);
+
+    if (strategy === STRATEGY_CACHEFIRST_LOCAL)
     {
         event.respondWith(
             cacheFirst({
                 request: event.request,
                 preloadResponsePromise: event.preloadResponse,
-                fallbackUrl: "/data/card-not-found-generic",
+                fallbackUrl: "",
             })
+        );
+    }
+    else if (strategy === STRATEGY_CACHEFIRST_IMAGE)
+    {
+        event.respondWith(
+            cacheCardImageFirst({
+                request: event.request,
+                preloadResponsePromise: event.preloadResponse,
+            })
+        );
+    }
+    else if (strategy === STRATEGY_NETWORKFIRST)
+    {
+        event.respondWith(
+            networkFirst(event.request, event.preloadResponse)
         );
     }
 }
 
+const isValidCache = function(name)
+{
+    return name === CACHE_CARDS || name === CACHE_NAME;
+}
+
+/**
+ * Invalidate records matchinf actual version
+ *
+ * @param {Cache} caches
+ * @returns {Promise}
+ */
+async function clearObsoleteCaches( caches ) 
+{
+    let removed = 0;
+    const keys = await caches.keys();
+    for (let key in keys)
+    {
+        continue;
+        if (!isValidCache(key))
+        {
+            console.info("Clearing obsolete cache", key);
+            await caches.delete(key);
+            removed++;
+        }
+    }
+
+    console.info(removed + " obsolete chaches");
+}
+
+const prepareCaches = async function(caches)
+{
+    const regularCache = await caches.open(CACHE_NAME);
+    const cardCache = await caches.open(CACHE_CARDS);
+
+    if (assets.length > 0)
+        await regularCache.addAll(assets);
+}
 /**
  * Register listeners
  */
-self.addEventListener("install", installEvent => installEvent.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)).catch(console.warn)));
+self.addEventListener("install", installEvent => {
+    // installEvent.waitUntil(prepareCaches(caches).catch(console.warn));
+});
+
+/** remove old caches */
+self.addEventListener( "activate", (event) => {
+    event.waitUntil(clearObsoleteCaches(caches));
+});
+
 self.addEventListener("fetch", fetchListener);
+
+
