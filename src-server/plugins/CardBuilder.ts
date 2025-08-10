8bd5184fe1361dac816c4522f261737186099e2a
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2025-08-10 10:10:01 +0100

    update relased state

--- src-server/plugins/CardBuilder.ts
+++ src-server/plugins/CardBuilder.ts
@@ -64,7 +64,7 @@ export default class CardBuilder
                     ice: map[key].ice !== false,
                     code: key.toUpperCase(),
                     dc: map[key].ice !== true,
-                    released: map[key].ice === true || map[key].released === true, 
+                    released: map[key].ice !== false || map[key].released === true, 
                     order: map[key].order ?? 100
                 };
             }
