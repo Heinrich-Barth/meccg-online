478f94e57a125a45e167960ddb996ac20031a4da
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2025-10-28 07:55:36 +0000

    remove space

--- src-client/game-client/game/table.js
+++ src-client/game-client/game/table.js
@@ -167,7 +167,7 @@
         div.setAttribute("id", "view_card_list_container");
         div.setAttribute("class", "hidden");
         div.innerHTML = `
-            <div class="view-card-list-container blue-box" data-class="view-card-list-container blue-box" >
+            <div class="view-card-list-container blue-box" data-class="view-card-list-container blue-box">
                 <div class="container-title-bar blue-box smallCaps">
                     <div class="container-title-bar-title fl"></div>
                     <div class="container-title-bar-reveal hideOnOffer fl"><i class="fa fa-eye"></i> <a href="#" title="${Dictionary.get("title_showtoopp", "Show to your Opponent")}" data-type=""> ${Dictionary.get("reval_opp", "reveal to opp.")}</a></div>
